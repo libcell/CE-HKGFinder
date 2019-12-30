c20659b1fe486a99909a2c810adb4a8873614a70
Bo Lee <libcell@cqu.edu.cn>
2019-12-30 22:38:57 +0800

    Update Chunk12-Validation of housekeeping using the DNA microarray datasets.R

--- Chunk12-Validation of housekeeping using the DNA microarray datasets.R
+++ Chunk12-Validation of housekeeping using the DNA microarray datasets.R
@@ -231,50 +231,199 @@ sort(apply(eset.mat, 1, ineq::Gini))
 ### End of GSE108968. =======================================================###
 
 
+
+################################################################################
+################################################################################
+
+library(affy)
+
+setwd("GSE118294_RAW/")
+
+dat <- ReadAffy()
+
+eset <- rma(dat)
+
+es <- exprs(eset)
+
+setwd("..")
+
+library(GEOquery)
+
+dat <- getGEO("GSE118294", GSEMatrix = TRUE)
+
+anno.tab <- dat[[1]]@featureData@data; dim(anno.tab)
+
+eset <- es; dim(eset) 
+
+table(anno.tab$ID == rownames(eset))
+
+probe.num <- nrow(anno.tab)
+
+all.gene <- c("cyc-1", "tba-1", "atp-3", "mdh-1", "gpd-2", 
+              "eif-3.C", "act-1", "cdc-42", "pmp-3", "act-2", 
+              "csq-1", "ama-1", "rbd-1", "rps-23", "rps-27", 
+              "rps-26", "rps-4", "rps-2", "rps-16", "rps-17", 
+              "rpl-24.1", "rpl-15", "rpl-35", "rpl-36", "rpl-33", "rpl-27")
+
+eset.mat <- NULL
+
+for (g in all.gene) {
+  
+  gene.pos <- NULL
+  
+  for (i in 1:probe.num) {
+    
+    gene.hit <- which(strsplit(anno.tab$`Gene Symbol`[i], " /// ")[[1]] == g)
+    
+    if (length(gene.hit) != 0) gene.pos <- c(gene.pos, i)
+    
+  }
+  
+  # gene.pos
+  
+  tmp <- eset[gene.pos, ]
+  
+  # print(tmp)
+  
+  if (length(gene.pos) > 1) gene.eset <- apply(as.matrix(tmp), 2, mean) else gene.eset <- tmp
+  
+  print(g)
+  
+  print(gene.eset)
+  
+  print("##################################################################")
+  
+  eset.mat <- rbind(eset.mat, gene.eset)
+  
+}
+
+rownames(eset.mat) <- all.gene
+
+boxplot(t(eset.mat))
+
+# SD. 
+
 sort(apply(eset.mat, 1, sd))
 
-library(ineq)
+# GC. 
 
-sort(apply(eset.mat, 1, Gini))
+sort(apply(eset.mat, 1, ineq::Gini))
 
-
-str(dat)
-
-class(dat)
-
-dim(exprs(dat))
-
-mdat<-getDee2Metadata("celegans")
-
-mdat[which(mdat$GSE_accession %in% "GSE94879"), ]
+### End of GSE118294 ======================================================= ###
 
 
-BiocManager::install("scater")
+library(limma)
+
+#. dat <- read.maimages(files = dir("GSE108968_RAW"),
+#.                      source="agilent", 
+#.                      columns = list(G = "gMedianSignal", 
+#.                                     Gb = "gBGMedianSignal", 
+#.                                     R = "gMedianSignal", 
+#.                                     Rb = "gBGMedianSignal"), 
+#.                      annotation = c("Row", 
+#.                                     "Col",
+#.                                     "FeatureNum",
+#.                                     "ControlType",
+#.                                     "ProbeName",
+#.                                     "GeneName",
+#.                                     "SystematicName"))
+
+dat <- get(load("GSE1108968.RData"))
+
+dat2 <- backgroundCorrect(dat, "normexp", offset = 50)
+
+eset <- normalizeBetweenArrays(dat2$R, method = "quantile")
+
+eset2 <- log(eset, 2)
+
+rownames(eset2) <- dat$genes$ProbeName
+
+eset2 <- as.data.frame(eset2)
+
+setwd("..")
+
+
+write.csv(eset2,"GSE108968_expr.csv")
+
+dat.tmp <- eset2
+
+########################################################
+
+gse108968 <- getGEO("GSE108968", GSEMatrix = TRUE)
+
+str(gse108968)
+
+anno.tab <- gse108968[[1]]@featureData@data; dim(anno.tab)
+
+rownames(eset2) <- NULL
+
+eset <- eset2; dim(eset) 
+
+table(anno.tab$NAME == rownames(eset))
+
+nr <- nrow(anno.tab)
+
+# annotation <- read.csv("GPL10094_old_annotations.txt", sep = "\t", header = FALSE)[-(1:19),1:8]
+
+# dat <- read.csv("GSE108968_expr.csv")
+
+all.gene <- c("cyc-1", "tba-1", "atp-3", "mdh-1", "gpd-2", 
+              "eif-3.C", "act-1", "cdc-42", "pmp-3", "act-2", 
+              "csq-1", "ama-1", "rbd-1", "rps-23", "rps-27", 
+              "rps-26", "rps-4", "rps-2", "rps-16", "rps-17", 
+              "rpl-24.1", "rpl-15", "rpl-35", "rpl-36", "rpl-33", "rpl-27")
+
+eset.mat <- NULL
+
+for (g in all.gene) {
+  
+  gene.pos <- NULL
+  
+  for (i in 1:nr) {
+    
+    gene.hit <- which(strsplit(anno.tab$GENE_SYMBOL[i], " /// ")[[1]] == g)
+    
+    if (length(gene.hit) != 0) gene.pos <- c(gene.pos, i)
+    
+  }
+  
+  # gene.pos
+  
+  tmp <- eset[gene.pos, ]
+  
+  # print(tmp)
+  
+  if (length(gene.pos) > 1) gene.eset <- apply(as.matrix(tmp), 2, mean) else 
+    
+    if (length(gene.pos) == 0) gene.eset <- 0 
+  
+  else gene.eset <- tmp
+  
+  print(g)
+  
+  print(gene.eset)
+  
+  print("##################################################################")
+  
+  eset.mat <- rbind(eset.mat, gene.eset)
+  
+}
+
+rownames(eset.mat) <- all.gene
+
+boxplot(t(eset.mat))
+
+# SD. 
+
+sort(apply(eset.mat, 1, sd))
+
+# GC. 
+
+sort(apply(eset.mat, 1, ineq::Gini))
 
 
 
 
-data("sc_example_counts")
-data("sc_example_cell_info")
-example_sce <- SingleCellExperiment(
-  list(counts = sc_example_counts), 
-  colData = sc_example_cell_info)
 
-cpm(example_sce) <- calculateCPM(example_sce, use_size_factors = FALSE)
 
-y <- matrix(rnbinom(20,size=1,mu=10),5,4)
-cpm(y)
-
-d <- DGEList(counts=y, lib.size=1001:1004)
-cpm(d)
-cpm(d,log=TRUE)
-
-d$genes <- data.frame(Length=c(1000,2000,500,1500,3000))
-rpkm(d)
-
-cpmByGroup(d, group=c(1,1,2,2))
-
-rpkmByGroup(d, group=c(1,1,2,2))
-
-library(SingleCellExperiment)
 
