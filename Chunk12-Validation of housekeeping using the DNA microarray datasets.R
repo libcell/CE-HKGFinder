117b044a9aa9ac742dcdfbcab736f3855e5a4fec
Bo Lee <libcell@cqu.edu.cn>
2019-12-30 09:01:57 +0800

    Update Chunk12-Validation of housekeeping using the DNA microarray datasets.R

--- Chunk12-Validation of housekeeping using the DNA microarray datasets.R
+++ Chunk12-Validation of housekeeping using the DNA microarray datasets.R
@@ -14,69 +14,8 @@
 ### code chunk number 12: Validation of HKGs using DNA microarray datasets.
 ### ****************************************************************************
 
-### For DNA microarray datasets. 
-
-library(GEOquery)
-
-dat <- getGEO("GSE118294", GSEMatrix = TRUE)
-
-dat <- dat[[1]]
-
-anno.tab <- dat@featureData@data; dim(anno.tab)
-
-eset <- exprs(dat); dim(eset) 
-
-table(anno.tab$ID == rownames(eset))
-
-probe.num <- nrow(anno.tab)
-
-all.gene <- c("cyc-1", "tba-1", "atp-3", "mdh-1", "gpd-2", 
-              "eif-3.C", "act-1", "cdc-42", "pmp-3", "act-2", 
-              "csq-1", "ama-1", "rbd-1", "rps-23", "rps-27", 
-              "rps-26", "rps-4", "rps-2", "rps-16", "rps-17", 
-              "rpl-24.1", "rpl-15", "rpl-35", "rpl-36", "rpl-33", "rpl-27")
-
-eset.mat <- NULL
-
-for (g in all.gene) {
-  
-  gene.pos <- NULL
-  
-  for (i in 1:probe.num) {
-    
-    gene.hit <- which(strsplit(anno.tab$`Gene Symbol`[i], " /// ")[[1]] == g)
-    
-    if (length(gene.hit) != 0) gene.pos <- c(gene.pos, i)
-    
-  }
-  
-  # gene.pos
-  
-  tmp <- eset[gene.pos, ]
-  
-  # print(tmp)
-  
-  if (length(gene.pos) > 1) gene.eset <- apply(as.matrix(tmp), 2, mean) else gene.eset <- tmp
-
-  print(g)
-  
-  print(gene.eset)
-  
-  print("##################################################################")
-  
-  eset.mat <- rbind(eset.mat, gene.eset)
-  
-}
-
-rownames(eset.mat) <- all.gene
-
-boxplot(t(eset.mat))
-
-
-
-### =======================================================================-- ##
-
-### For DNA microarray datasets. 
+### ======================================================================== ###
+###--For DNA microarray dataset, GSE76380.-----------------------------------### 
 
 library(GEOquery)
 
@@ -137,9 +76,90 @@ rownames(eset.mat) <- all.gene
 
 boxplot(t(eset.mat))
 
-### =======================================================================-- ##
+# SD. 
 
-### For DNA microarray datasets. 
+sort(apply(eset.mat, 1, sd))
+
+# GC. 
+
+sort(apply(eset.mat, 1, ineq::Gini))
+
+### End of GSE76380 =========================================================###
+
+### ======================================================================== ###
+###--For DNA microarray dataset, GSE118294.----------------------------------### 
+
+library(GEOquery)
+
+dat <- getGEO("GSE118294", GSEMatrix = TRUE)
+
+dat <- dat[[1]]
+
+anno.tab <- dat@featureData@data; dim(anno.tab)
+
+eset <- exprs(dat); dim(eset) 
+
+table(anno.tab$ID == rownames(eset))
+
+probe.num <- nrow(anno.tab)
+
+all.gene <- c("cyc-1", "tba-1", "atp-3", "mdh-1", "gpd-2", 
+              "eif-3.C", "act-1", "cdc-42", "pmp-3", "act-2", 
+              "csq-1", "ama-1", "rbd-1", "rps-23", "rps-27", 
+              "rps-26", "rps-4", "rps-2", "rps-16", "rps-17", 
+              "rpl-24.1", "rpl-15", "rpl-35", "rpl-36", "rpl-33", "rpl-27")
+
+eset.mat <- NULL
+
+for (g in all.gene) {
+  
+  gene.pos <- NULL
+  
+  for (i in 1:probe.num) {
+    
+    gene.hit <- which(strsplit(anno.tab$`Gene Symbol`[i], " /// ")[[1]] == g)
+    
+    if (length(gene.hit) != 0) gene.pos <- c(gene.pos, i)
+    
+  }
+  
+  # gene.pos
+  
+  tmp <- eset[gene.pos, ]
+  
+  # print(tmp)
+  
+  if (length(gene.pos) > 1) gene.eset <- apply(as.matrix(tmp), 2, mean) else gene.eset <- tmp
+
+  print(g)
+  
+  print(gene.eset)
+  
+  print("##################################################################")
+  
+  eset.mat <- rbind(eset.mat, gene.eset)
+  
+}
+
+rownames(eset.mat) <- all.gene
+
+boxplot(t(eset.mat))
+
+# SD. 
+
+sort(apply(eset.mat, 1, sd))
+
+# GC. 
+
+sort(apply(eset.mat, 1, ineq::Gini))
+
+### End of GSE118294 ======================================================= ###
+
+
+
+
+### ======================================================================== ###
+###--For DNA microarray dataset, GSE108968.----------------------------------### 
 
 library(GEOquery)
 
@@ -199,6 +219,17 @@ for (g in all.gene) {
 rownames(eset.mat) <- all.gene
 
 boxplot(t(eset.mat))
+
+# SD. 
+
+sort(apply(eset.mat, 1, sd))
+
+# GC. 
+
+sort(apply(eset.mat, 1, ineq::Gini))
+
+### End of GSE108968. =======================================================###
+
 
 sort(apply(eset.mat, 1, sd))
 
